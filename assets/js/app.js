38258497286611fcab51d04b444a30aef062a594
siskafks <120354576+siskafks@users.noreply.github.com>
2023-07-07 11:29:25 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+let slideIndex = [1,1,1,1,1];
+let slideId = ["slides1", "slides2", "slides3","slides4","slides5"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
